d6d175e84500f80251200e7b5e02387f530ccefd
ShaheerJamalChishti09 <shaheerjamal369@gmail.com>
2025-04-24 16:42:44 +0500

    Scrapped Literature Books

---
+++ Web Scraping/Assignment 1/Sample Submission/ShaheerJamal/main 03.py
@@ -0,0 +1,35 @@
+from bs4 import BeautifulSoup
+import requests
+import re
+import json
+
+url = "https://www.thriftbooks.com/browse/?12529col#b.s=mostPopular-desc&b.p=1&b.pp=50&b.col&b.f.t%5B%5D=13901&b.list"
+web = requests.get(url).content
+soup = BeautifulSoup(web, 'lxml')
+
+content = soup.find_all("script")
+string = content[12].string
+# print(string)
+
+
+main_data = re.search(r'window\.searchStoreV2\s*=\s*(\{.*?\});', string, re.DOTALL)
+
+books_data = {}
+
+if main_data:
+    works = main_data.group(1)
+    works_json = json.loads(works)
+    works = works_json.get("works")
+    
+# print(data)
+for i in works:
+    books_data.update(i)
+    title = books_data["title"]
+    # author = books_data[""]
+    # price = books_data[""]
+    # rating = books_data[""]
+    # isbn = books_data[""]
+    
+    with open("Literature Books.csv", "a") as f:
+        f.write(f"{title}\n")
+        
